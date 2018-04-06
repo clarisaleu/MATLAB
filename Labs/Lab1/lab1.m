3ad8f01fb530f50ad845dd2bc1cd0ea0f26b263e
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-04-05 21:26:29 -0700

    Updated graphs

--- Labs/Lab1/lab1.m
+++ Labs/Lab1/lab1.m
@@ -1,13 +1,19 @@
 %Lab Assignment #1
-
-%Studying unforced oscillating systems. A dying spring
+%Studying unforced oscillating systems. A dying spring.
 
 A=10;
 k=.25;
 omega=2.5;
 phi=1.5;
+
 t=linspace(0,10);
 
-
+%Our function
 y=A.*exp(-k.*t).*sin(omega.*t+phi);
 
+%Plot
+plot(t,y,'-.dm');hold on;
+title({'Lab #1 Graph - Unforced Oscillating Systems';'y=Ae^{-kt}sin({\omega}t+{\phi})'})
+xlabel('t - Time (seconds)')
+ylabel('y - Postion (m)')
+axis([0 6*pi./omega -10 10])
--- Labs/Lab1/lab1plot.m
+++ Labs/Lab1/lab1plot.m
@@ -1,19 +1,17 @@
-%What effect does A have on the graph? What about k and omega?
+%Varying A changes the maximum amplitude of the spring (where you start
+%it) & Varying omega changes the period of oscillation
 
-%Varying  A changes the maximum amplitude of the spring (where you start
-%it)
+
 c=2;
+
+%Vary The Amplitude
 y2=c.*exp(-k.*t).*sin(omega.*t+phi);
+plot(t,y2,'--xg');hold on;
 
-plot(t,y,'color','r');hold on;
-plot(t,y2,'color','b');
-
-%Varying omega changes the period of oscillation
+%Varying Omega
 y3=A.*exp(-k.*t).*sin(c.*t+phi);
-plot(t,y3,'color','m');
+plot(t,y3,'-*c');
 
-title('Lab #1 Graph - Unforced Oscillating Systems')
-xlabel('x')
-ylabel('y')
+legend('y=10e^{-0.25t}sin(2.5t+1.5)','y=2e^{-0.25t}sin(2.5t+1.5)','y=10e^{-0.25t}sin(2t+1.5)')
 axis([0 6*pi./c -10 10])
 
