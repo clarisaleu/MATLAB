233c3aca417abf7cef6a2707704399c1a55d5b54
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-04-17 18:09:32 -0700

    added instructions etc

--- Labs/Lab3/taylorapproxoepsilon.m
+++ Labs/Lab3/taylorapproxoepsilon.m
@@ -1,7 +1,7 @@
 %A function that returns the taylor approximation of  e^x centered at x=0
 %to within epsilon of the true value.
 
-function y = taylorapproxepsilon(x,epsilon)
+function y = taylorapproxoepsilon(x,epsilon)
 if epsilon <0
    disp('Sorry hoss, epsilon must be positive. Try again!')
 else
